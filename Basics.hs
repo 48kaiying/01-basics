3f3ab4fed1b5c420110c19749d6e192264ce4ee9
unknown <48kaiying@gmail.com>
2022-09-05 17:41:40 -0400

    ans

--- Basics.hs
+++ Basics.hs
@@ -25,6 +25,8 @@ importing definitions from other modules.
 module Basics where
 
 -- library imports must come at the beginning
+
+import Data.Bits (Bits (xor))
 import Test.HUnit
   ( Counts,
     Test (TestList),
@@ -70,6 +72,7 @@ Try clicking on "Evaluate..." below.
 -}
 
 -- >>> 3 * (4 + 5)
+-- 27
 
 {-
 A Haskell module (like this one) is a list of *definitions*. These definitions
@@ -79,11 +82,17 @@ allow us to give names to Haskell expressions.
 ex :: Integer
 ex = 3 * (4 + 5)
 
+mp :: Float
+mp = 5 * 6
+
 {-
 We can ask VSCode to calculate these values, just as we did above.
 -}
 
 -- >>> ex
+-- 27
+-- >>> mp
+-- 30.0
 
 {-
 Whenever we give a name to an expression, it is a good idea to also write down
@@ -112,12 +121,14 @@ Compare the value of a extra-large `Integer`
 -}
 
 -- >>> bigInteger
+-- 12345678901234567890
 
 {-
 with an `Int`
 -}
 
 -- >>> bigInt
+-- -6101065172474983726
 
 {-
 Above, we declared the type of an expression separately from giving it a
@@ -126,6 +137,7 @@ still annotate it with its type using `::`.
 -}
 
 -- >>> 31 * (42 + 56) :: Integer
+-- 3038
 
 {-
 More generally, the type annotation can be attached to any subexpression, not
@@ -133,6 +145,7 @@ just at the top level.
 -}
 
 -- >>> (31 :: Integer) * (42 + 56)
+-- 3038
 
 {-
 It is good style to annotate the type of *every* declaration in a Haskell
@@ -157,20 +170,26 @@ using the same overloaded syntax.
 -}
 
 -- >>> 31 * (42 + 56) :: Double    -- double precision floating point
+-- 3038.0
 
 {-
 Furthermore, you'll also find characters, strings and boolean values.
 -}
 
 -- >>> 'a' :: Char                 -- characters
+-- 'a'
 
 -- >>> "abcd" :: String            -- strings
+-- "abcd"
 
 -- >>> "cis" ++ "552"              -- string concatenation
+-- "cis552"
 
 -- >>> True :: Bool                -- boolean values
+-- True
 
 -- >>> 1 <= 3 || False && 3 > 2    -- boolean operators, comparisons
+-- True
 
 {-
 What is a little different about Haskell is that everything is an expression,
@@ -179,6 +198,7 @@ expressions.
 -}
 
 -- >>> (if ex > 28 then 1 else 0) + 2 :: Int
+-- 2
 
 {-
 Now the last basic type, shown below, is subtle. It is a special constant,
@@ -189,6 +209,7 @@ this basic type is that there is only *one* value with type `()`.
 -}
 
 -- >>> () :: ()            -- 'unit' (both value and type have the same syntax)
+-- ()
 
 {-
 What is Abstraction?
@@ -218,12 +239,14 @@ We call functions by providing them with arguments.
 -}
 
 -- >>> pat 31 42 56
+-- 3038
 
 {-
 No parentheses are necessary, unless the argument itself is a compound expression.
 -}
 
 -- >>> pat (30 + 1) 42 56
+-- 3038
 
 {-
 The important question is not "What does this function do?"
@@ -324,6 +347,7 @@ error will trigger.
 -}
 
 -- >>> 1 + 2 + 3 + error "Here!"
+-- Here!
 
 {-
 However, we won't trigger an error that is in dead code, such as in
@@ -389,7 +413,7 @@ Thus:
 -}
 
 -- >>> const (error "Here!") 4
--- 3
+-- 4
 
 {-
 We'll see more examples of laziness throughout the semester. Sometimes we use the word "strictness" to
@@ -710,16 +734,19 @@ pat4 :: ((Int, Int), Int) -> Int
 pat4 ((a, b), c) = a * (b + c)
 
 -- >>> pat4 tup4
+-- 5
 
 pat5 :: (Int, (Int, Int)) -> Int
 pat5 (a, (b, c)) = a * (b + c)
 
 -- >>> pat5 tup5
+-- 5
 
 pat6 :: (Int, Int, Int) -> Int
 pat6 (a, b, c) = a * (b + c)
 
 -- >>> pat6 tup6
+-- 5
 
 {-
 We can stick anything in tuples, even IO actions.
@@ -767,7 +794,7 @@ This type describes either a value of type `A`, or else nothing.
 -}
 
 m1 :: Maybe Int
-m1 = Just 2 -- the 'Just' tag tells the compiler that we have a value
+m1 = Just 5 -- the 'Just' tag tells the compiler that we have a value
 
 m2 :: Maybe Int
 m2 = Nothing -- the 'Nothing' tag means there is no value
@@ -802,7 +829,8 @@ error if it is ever evaluated.
 -}
 
 jn' :: Maybe (Maybe a) -> Maybe a
-jn' = undefined
+jn' (Just x) = x
+jn' Nothing = Nothing
 
 {-
 'Maybe' is useful for partial functions
@@ -833,7 +861,7 @@ l1 :: [Double]
 l1 = [1.0, 2.0, 3.0, 4.0]
 
 l2 :: [Int]
-l2 = undefined -- make a list of numbers
+l2 = [1, 2, 3, 4] -- make a list of numbers
 
 {-
 Lists can contain structured data...
@@ -847,7 +875,7 @@ l3 = [(1, True), (2, False)]
 -}
 
 l4 :: [[Int]]
-l4 = undefined -- make a list of lists
+l4 = [[1], [2], [3], [4, 5]] -- make a list of lists
 
 {-
 List elements *must* have the same type.
@@ -877,6 +905,7 @@ What is the value of l7?
 -}
 
 -- >>> l7
+-- "hello 552!"
 --
 
 {-
@@ -906,14 +935,17 @@ Try evaluating `c1` and `c2`.
 -}
 
 -- >>> c1
+-- [True,False,False]
 --
 -- >>> c2
+-- [1]
 --
 
 {-
 And check out the type of `c3`.
 -}
 
+c3 :: [[a]]
 c3 = [] : []
 
 {-
@@ -940,8 +972,10 @@ Try evaluating `s1` and `s2`.
 -}
 
 -- >>> s1
+-- "abc"
 --
 -- >>> s2
+-- "abc"
 --
 
 {-
@@ -1001,7 +1035,7 @@ clone :: a -> Int -> [a]
 We implement this function by recursion on the integer argument.
 -}
 
-clone x n = if n <= 0 then [] else x : clone x (n -1)
+clone x n = if n <= 0 then [] else x : clone x (n - 1)
 
 {-
 **Step 4**: Run the tests.
@@ -1058,7 +1092,7 @@ range :: Int -> Int -> [Int]
 **Step 3**: Define the function. This part is for you to do for your quiz.
 -}
 
-range i j = undefined
+range i j = if i > j then [] else i : range (i + 1) j
 
 {-
 **Step 4**: Run the tests.
@@ -1106,7 +1140,9 @@ lists that have three or more elements.
 -}
 
 isLong :: [a] -> Bool
-isLong = undefined
+isLong [_, _, _] = True
+isLong (_ : _ : _ : [_]) = True
+isLong _ = False
 
 testIsLong :: Test
 testIsLong =
@@ -1114,7 +1150,8 @@ testIsLong =
     [ not (isLong []) ~? "nil", -- can convert booleans to tests by naming them via `~?`
       not (isLong "a") ~? "one",
       not (isLong "ab") ~? "two",
-      isLong "abc" ~? "three"
+      isLong "abc" ~? "three",
+      isLong "abcd" ~? "four"
     ]
 
 {-
@@ -1254,7 +1291,8 @@ listIncrTests :: Test
 listIncrTests =
   TestList
     [ listIncr [1, 2, 3] ~?= [2, 3, 4],
-      listIncr [42] ~?= [43]
+      listIncr [42] ~?= [43],
+      listIncr [] ~?= []
     ]
 
 {-
@@ -1266,7 +1304,8 @@ listIncr :: [Int] -> [Int]
 **Step 3**: Define the function.
 -}
 
-listIncr = undefined
+listIncr [] = []
+listIncr (x : xs) = (x + 1) : listIncr xs
 
 {-
 **Step 4**: Run the tests.
@@ -1289,7 +1328,10 @@ listAddTests :: Test
 listAddTests =
   TestList
     [ listAdd [1, 2, 3] [2, 4, 5] ~?= [3, 6, 8],
-      listAdd [42] [] ~?= []
+      listAdd [42] [] ~?= [],
+      listAdd [] [67] ~?= [],
+      listAdd [1, 2, 3, 6] [2, 4, 5] ~?= [3, 6, 8],
+      listAdd [1, 2, 3] [2, 4, 5, 7] ~?= [3, 6, 8]
     ]
 
 {-
@@ -1301,7 +1343,9 @@ listAdd :: [Int] -> [Int] -> [Int]
 **Step 3**: Define the function.
 -}
 
-listAdd = undefined
+listAdd [] _ = []
+listAdd _ [] = []
+listAdd (x : xs) (y : ys) = (x + y) : listAdd xs ys
 
 {-
 **Step 4**: Run the tests.
@@ -1311,7 +1355,7 @@ runLAddTests :: IO Counts
 runLAddTests = runTestTT listAddTests
 
 -- >>> runLAddTests
--- Counts {cases = 2, tried = 2, errors = 0, failures = 0}
+-- Counts {cases = 5, tried = 5, errors = 0, failures = 0}
 
 {-
 Function practice: "Infinite" lists
